e0e8490647223e48833728d19de31f5925275a90
Thao Tran <thaophuongtran@chotot.vn>
2018-10-14 13:54:27 +0700

    finish hackathon

--- front-end/galaxy-hackathon/src/components/News/index.js
+++ front-end/galaxy-hackathon/src/components/News/index.js
@@ -1,79 +1,32 @@
 import React from 'react';
 import './styles.css';
 import {Image, Row} from 'react-bootstrap';
-import { Image } from 'react-bootstrap';
 import axios from 'axios'
 
-const News = () => {
-  return(
-    <div className='container-news'>
-      <div
-        className='news-header'
-      >
-        <div className='news-info'>
-          <Image
-            className='news-avt'
-            src="http://sohanews.sohacdn.com/thumb_w/660/2016/7-avatar-1478435729759-0-214-355-786-crop-1478435953995.jpg"
-            circle
-          />
-          <span className='news-authorname'>Phuong Thao</span>
-        </div>
-        <div className='news-title'>
-        <span >
-          Biến đổi khí hậu tại miền Trung
-        </span>
-        </div>
-      </div>
-      <div
-        className='news-body'
-      >
-        <span>
-          Hiện nay, vấn đề ô nhiễm môi trường đang là chủ đề nóng trên các mặt báo và nhận được rất nhiều sự quan tâm của người dân...
-
-        <a>Xem tiếp ></a>
-        </span>
-        <Image src="https://kenh14cdn.com/2017/screen-shot-2017-04-02-at-2-20-29-pm-1491117661107.png" rounded />
-        <div style={{marginTop:10}}>
-          <img src='https://cdn4.iconfinder.com/data/icons/shopping-b/512/YPS__tag_B-512.png' style={{width:'20px', height: '20px'}}/>
-          <a><span className='tag'> Đồng bằng sông Cửu Long</span></a>
-          <a><span className='tag'> Lũ lụt</span></a>
-        </div>
-        <div style={{float:'right'}}>
-        </div>
-      </div>
-    </div>
-  )
-};
-
-export default News;
 
   export default class News extends React.Component {
     state = {
       news: [],
       user: null,
-    }
+    };
 
     componentWillMount() {
-      console.log('==willmount');
       axios.get('http://localhost:3001/api/feed')
         .then(res => {
           const news = res.data.feeds;
-          console.log('==result',news);
           this.setState({ news: news });
-        }).catch(err => console.log('==err', err))
+        }).catch(err => console.log('==err', err));
 
       let url = 'http://localhost:3001/api/user/' + this.props.no;
       console.log(url);
       axios.get(url)
         .then(res => {
           const user = res.data.user;
-          console.log('aaaaaaaaaaa',user)
           this.setState({ user: user });
         }).catch(err => console.log('==err', err))
     }
 
     render() {
-      console.log('==render', this.state.news);
       let username = "...";
       let content = "..";
       let title = ".";
@@ -113,8 +66,15 @@ export default News;
             className='news-body'
           >
             <span>
-              {content}
+              {content}...
+              <a>Xem tiếp ></a>
             </span>
+            <Image src="https://kenh14cdn.com/2017/screen-shot-2017-04-02-at-2-20-29-pm-1491117661107.png" rounded />
+            <div style={{marginTop:10}}>
+              <img src='https://cdn4.iconfinder.com/data/icons/shopping-b/512/YPS__tag_B-512.png' style={{width:'20px', height: '20px'}}/>
+              <a><span className='tag'> Đồng bằng sông Cửu Long</span></a>
+              <a><span className='tag'> Lũ lụt</span></a>
+            </div>
           </div>
         </div>
       )
